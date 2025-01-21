3d4e06e018cdc25638499ecbcd3fff6307b2a890
ywow-ai <masyaqin37@gmail.com>
2025-01-21 17:11:08 +0700

    feat: integrate tools and function call handling in chat flow

--- index.ts
+++ index.ts
@@ -1,15 +1,32 @@
 import { cancel, isCancel, outro, spinner, text } from "@clack/prompts";
 import {
   GoogleGenerativeAI,
+  SchemaType,
   type StartChatParams,
+  type Tool,
 } from "@google/generative-ai";
 
 (async () => {
   let chatState: boolean = true;
   const genAi = new GoogleGenerativeAI(process.env.GEMINI_API_KEY ?? "");
+  const tools: Tool[] = [
+    {
+      functionDeclarations: [
+        {
+          name: "echo-x",
+          description: "belum aku set",
+        },
+      ],
+      codeExecution: () => {
+        console.log("xxx-ai-");
+      },
+    },
+  ];
+
   const chat = genAi
     .getGenerativeModel({
       model: "gemini-1.5-flash",
+      tools,
       systemInstruction: {
         role: "system",
         parts: [{ text: process.env.SYSTEM_PROMPT ?? "" }],
@@ -48,6 +65,16 @@ import {
     s.start("Waiting");
     const { response } = await chat.sendMessage(prompt);
     s.stop("Gemini");
+
+    const funcs = response.functionCalls();
+    if (funcs) {
+      for (const fn of funcs) {
+        fn.name;
+        tools.find((x) => {
+          x;
+        });
+      }
+    }
     outro(response.text());
   }
 })().catch(console.error);
